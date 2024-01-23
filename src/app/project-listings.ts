4052344cd034db2685ed67382ecee1f73409e6f0
Agreer1409 <greer.austin1409@gmail.com>
2024-01-23 10:03:48 -0500

    adding listing of projects

---
+++ src/app/project-listings.ts
@@ -0,0 +1,7 @@
+// An interface is a contract from us and the data. It can help with making sure our code can expect the correct types.
+export interface ProjectListings {
+  title: string,
+  startdate: string,
+  enddate: string,
+  owner: string,
+}
